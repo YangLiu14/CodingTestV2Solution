3a47ac3a8516c5be87c4e5148d763e73380bb51e
YangLiu14 <lylander14@gmail.com>
2022-12-12 01:08:07 +0800

    template for slidingWindow

--- labuladong模版/3_滑动窗口.cpp
+++ labuladong模版/3_滑动窗口.cpp
@@ -1,3 +1,39 @@
-//
-// Created by kloping on 2022/12/11.
-//
+#include <iostream>
+#include <unordered_map>
+using namespace std;
+
+/**
+ * 滑动窗口一般是用来应对【子串问题】
+ * 但是里面有许多细节：
+ * 1. 如何向窗口添加新元素
+ * 2. 如何缩小窗口
+ * 3. 在窗口滑动的哪个阶段更新结果
+ */
+
+void slidingWindow(string s) {
+    unordered_map<char, int> window;
+    int left = 0, right = 0;
+
+    while (right < s.size()) {
+        // c是将要移入窗口的字符
+        char c = s[right];
+        // 增大窗口
+        right++;
+        // 进行窗口内数据的一系列更新
+        /** ... **/
+
+        /*** debug ***/
+        printf("window: [%d, %d]\n", left, right);
+        /*** End of debug ***/
+
+        // 判断左侧窗口是否要收缩
+        while (/* window needs to shrink */) {
+            // d 是将要被移出窗口的字符
+            char d = s[left];
+            // 缩小窗口
+            left++;
+            // 进行窗口内数据的一系列更新
+            /** ... **/
+        }
+    }
+}
