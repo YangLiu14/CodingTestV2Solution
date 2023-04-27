6508061fa9bad78101ed76b923dbb2ce2c92466a
Kloping <kloping@KlopingdeMacBook-Pro.local>
2023-04-27 17:46:29 +0800

    add more example for sort (c++)

--- 常见算法/sort.cpp
+++ 常见算法/sort.cpp
@@ -5,10 +5,19 @@
 using namespace std;
 
 
+// Sort by Criterion
+void sortByLen(vector<string>& words) {
+    // words里包含一些string，按照string的长度，由大到小进行排序
+    sort(words.begin(), words.end(), [](const string &a, const string &b) {
+            return a.size() < b.size();
+    });
+}
+
+
 // 自定义sort算法
 // 此sort达到的效果是: 按照heights排序，但是同时也把heights对应的indices
 // 也按照heights排序后的变动跟着变动了
-vector<int> sortByHeights(vector<int> heights) {
+void sortByHeights(vector<int> heights) {
     int n = heights.size();
 
     // Create a vector of indices from 0 to n-1
